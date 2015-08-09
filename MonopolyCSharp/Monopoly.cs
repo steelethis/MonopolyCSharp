85196b0e9a64f3d55fca3cd6e61334c991191287
Josh Steele <josh@steelethis.com>
2015-08-09 00:35:31 -0400

    All tests for Player Movement passed.  Player creation fully tested. Currently working on player order.

--- MonopolyCSharp/Monopoly.cs
+++ MonopolyCSharp/Monopoly.cs
@@ -9,6 +9,8 @@ namespace MonopolyCSharp
     public class Monopoly
     {
         public int BoardSize { get; private set; }
+        public Queue<Player> PlayerQueue { get; private set; }
+        public List<Player> PlayerList { get; private set; }
 
         // Random seed for the game.
         private Random random;
@@ -17,8 +19,41 @@ namespace MonopolyCSharp
         {
             random = new Random();
             BoardSize = 40;
+        }
+
+        public void CreatePlayers(List<string> players)
+        {
+            PlayerList = new List<Player>();
+
+            if (players.Count < 2 || players.Count > 8)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            foreach (string player in players)
+            {
+                PlayerList.Add(new Player(this, player));
+            }
+        }
+
+        public void CreatePlayerOrder()
+        {
+            PlayerQueue = new Queue<Player>();
+
+            Dictionary<Player, int> playerRolls = new Dictionary<Player, int>();
+
+            foreach(Player player in PlayerList)
+            {
+                playerRolls.Add(player, RollDice());
+            }
+
+            // TODO: Sort players by their roll and put them in a queue in that order.
 
         }
 
+        public int RollDice()
+        {
+            return random.Next(1, 13);
+        }
     }
 }
